0a4792a72306074e1f101cc4ada8c93dcdb811d2
fleaz <mail@felixbreidenstein.de>
2023-01-10 00:20:32 +0100

    Init

---
+++ generate.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+from jinja2 import Environment, FileSystemLoader
+import yaml
+
+
+env = Environment( loader = FileSystemLoader("."))
+template = env.get_template('template.j2')
+
+with open("data.yaml","rb") as fh:
+    data = yaml.safe_load(fh)
+
+with open("index.html", 'w') as fh:
+    fh.write(template.render(data = data))
